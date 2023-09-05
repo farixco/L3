ab40b744862cf5bb6c13f93f06966d342dcaa2d9
farixco <farodriguezor@correo.url.edu.gt>
2023-09-04 21:47:19 -0600

    Bugs removidos. Finalizado

--- E2/main.cpp
+++ E2/main.cpp
@@ -3,7 +3,7 @@
 int main() {
    int diaIni, bisiesto = -1;
    std::string intro;
-   std::cout << "Ingrese el día en el que inicia el año." << std::endl;
+   std::cout << "Ingrese el (número del) día en el que inicia el año. Rango aceptable: 1 a 7." << std::endl;
    while (diaIni >= 8 || diaIni <= 0) {
       std::cin >> intro;
       diaIni = std::stoi(intro);
@@ -15,10 +15,10 @@ int main() {
       bisiesto = std::stoi(intro);
    }
    int*** calendario = new int**[12];
-   // inicializando, 12 meses x 5 semanas x 7 días
+   // inicializando, 12 meses x 6 semanas x 7 días
    for (int i = 0; i < 12; ++i) {
-      *(calendario + i) = new int*[5];
-      for (int j = 0; j < 5; ++j) {
+      *(calendario + i) = new int*[6];
+      for (int j = 0; j < 6; ++j) {
 	 *(*(calendario + i) + j) = new int[7];
 	 for (int k = 0; k < 7; ++k) {
 	    *(*(*(calendario + i) + j) + k) = 0;
@@ -64,21 +64,53 @@ int main() {
 	 }
 	 if (k >= 7) {
 	    k = 0;
-	    ++j;
+	       ++j;
 	 }
 	 calendario[i][j][k] = dia;
 	 ++dia;
 	 ++k;
       }
-      diaIni = 7 - k; 
+      if (k < 7) {
+	 diaIni = k;
+      } else {
+	 diaIni = 0;
+      }
    }
    // print time
    for (int i = 0; i < 12; ++i) {
-      for (int j = 0; j < 5; ++j) {
+      switch (i) {
+	 case 0: std::cout << "Enero" << std::endl;
+	    break;
+	 case 1: std::cout << "Febrero" << std::endl;
+	    break;
+	 case 2: std::cout << "Marzo" << std::endl;
+	    break;
+	 case 3: std::cout << "Abril" << std::endl;
+	    break;
+	 case 4: std::cout << "Mayo" << std::endl;
+	    break;
+	 case 5: std::cout << "Junio" << std::endl;
+	    break;
+	 case 6: std::cout << "Julio" << std::endl;
+	    break;
+	 case 7: std::cout << "Agosto" << std::endl;
+	    break;
+	 case 8: std::cout << "Septiembre" << std::endl;
+	    break;
+	 case 9: std::cout << "Octubre" << std::endl;
+	    break;
+	 case 10: std::cout << "Noviembre" << std::endl;
+	    break;
+	 case 11: std::cout << "Diciembre" << std::endl;
+	    break;
+      }
+      for (int j = 0; j < 6; ++j) {
 	 for (int k = 0; k < 7; ++k) {
-            if (calendario[i][j][k] != 0) {
+            if (calendario[i][j][k] >= 10) {
                std::cout << calendario[i][j][k] << " ";
-            } else {
+            } else if (calendario[i][j][k] < 10 && calendario[i][j][k] > 0) {
+	       std::cout << " " << calendario[i][j][k] << " ";
+	    } else {
 	       std::cout << "   ";
 	    }
          }
